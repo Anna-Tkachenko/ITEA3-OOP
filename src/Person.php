84464e81cfe2a6fc8e0b227936334b52e0396135
Anna Tkachenko <tkachenko.anna835@gmail.com>
2018-11-29 20:10:52 +0200

    Add console_helper

--- src/Person.php
+++ src/Person.php
@@ -11,4 +11,14 @@ class Person
     public $username;
     public $email;
     public $age;
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
 }
---
+++ src/console_helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tkachenko
+ * Date: 11/29/18
+ * Time: 8:01 PM
+ */
+
+function writeln($content)
+{
+    echo $content . \PHP_EOL;
+}
